9b2724854c754b01d1d543580cf0c83d4176c11d
flakedev <matt.me.jam@gmail.com>
2016-11-13 14:45:44 +0000

    Sync

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var background: UIImageView!
+    
+    @IBOutlet weak var titleImage: UIImageView!
+
+    @IBOutlet weak var welcomeBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("Hello World")
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func welcomePressed(_ sender: Any) {
+        
+        background.isHidden = true
+        titleImage.isHidden = false
+        welcomeBtn.isHidden = true
+        
+    }
 
 }
 
